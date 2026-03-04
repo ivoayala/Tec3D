29622c78ca5b038003d3c6b46b2d76b3d9290145
Ivo A <29932293+ivoayala@users.noreply.github.com>
2026-03-04 10:05:39 -0600

    Update MQTT_Tec3D_git_TeamA.ino

--- MQTT_Tec3D_git_TeamA.ino
+++ MQTT_Tec3D_git_TeamA.ino
@@ -14,8 +14,8 @@
 
 // Wifi security
 
-const char* ssid = "Tec-IoT";
-const char* password = "spotless.magnetic.bridge";
+const char* ssid = "Tec3D";
+const char* password = "12345678";
 
 
 // MQTT Broker IP address
@@ -148,3 +148,4 @@ void loop() {
 
     }
 }
+
